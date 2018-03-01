7fb90e8407bfb5ccc00a756832b140f95a68fa52
Tiago Maritan <tiagomaritan@lavid.ufpb.br>
2018-03-01 09:08:16 -0300

    Alterando arquivo alomundo.cpp para incluir uma nova mensagem de saida

--- alomundo.cpp
+++ alomundo.cpp
@@ -4,5 +4,7 @@ int main(){
 
 	std::cout << "Alo mundo" << std::endl;
 	std::cout << "Modificando arquivo original" << std::endl;
+	
+	std::cout << "Alterando so pra mandar pro GIT" << std::endl;
 	return 0;
 }
